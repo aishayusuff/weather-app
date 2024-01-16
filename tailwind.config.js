2253c4dac4fb7301d712515b36870d3c4071d683
aishayusuff <aishatyusuffe@gmail.com>
2024-01-16 23:10:32 +0100

    added some custom themes to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: { fontFamily: {
+      'sans': ['Fira Sans', 'sans-serif'],
+      },
+      colors: {
+        'backdrop-blue': '#07253d',
+        'font-pink': '#e91e63',
+      },
+    },
   },
   plugins: [],
 };
