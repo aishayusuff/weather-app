8106764c5d7f712fa07d4b0f47f8b9dac88bbcc3
aishayusuff <aishatyusuffe@gmail.com>
2024-02-17 22:22:05 +0100

    removes apiKey leak and adds transition

--- src/script.js
+++ src/script.js
@@ -1,25 +1,39 @@
-const apiKey = "c9904cf1b5886b2c9ca6da7b370dad0c";
 const city = "Doha";
 const url = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=metric`;
 
 const searchInput = document.querySelector(".search");
 const searchBtn = document.querySelector(".search-btn");
+const cityEl = document.querySelector(".city");
+const tempEl = document.querySelector(".temp");
+const humidityEl = document.querySelector(".humidity");
+const windEl = document.querySelector(".wind");
 
 const checksWeather = async function (city) {
   const response = await fetch(`${url}&q=${city}`);
   const data = await response.json();
 
-  //updates html values with dynamic data
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = `${Math.round(data.main.temp)}°C`;
-  document.querySelector(".humidity").innerHTML = `${data.main.humidity}%`;
-  document.querySelector(".wind").innerHTML = `${data.wind.speed} km/h`;
+  //Adds opacity to target elements for transition effects
+  cityEl.classList.add("opacity-0");
+  tempEl.classList.add("opacity-0");
+  humidityEl.classList.add("opacity-0");
+  windEl.classList.add("opacity-0");
+
+  setTimeout(() => {
+    //updates html values with dynamic data
+    cityEl.innerHTML = data.name;
+    tempEl.innerHTML = `${Math.round(data.main.temp)}°C`;
+    humidityEl.innerHTML = `${data.main.humidity}%`;
+    windEl.innerHTML = `${data.wind.speed} km/h`;
+
+    //removes opacity
+    cityEl.classList.remove("opacity-0");
+    tempEl.classList.remove("opacity-0");
+    humidityEl.classList.remove("opacity-0");
+    windEl.classList.remove("opacity-0");
+  }, 1000);
 };
 
 //Updates checksWeather function with city searched in input
 searchBtn.addEventListener("click", () => {
-  console.log(searchInput.value);
   checksWeather(searchInput.value);
 });
-
-checksWeather("Ibadan");
